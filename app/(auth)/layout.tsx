987fd3caf0d90debb1ceef914e64bb26818d1028
Avery Yang <averyyang2674@gmail.com>
2023-09-11 18:28:24 -0500

    fixed issues with ClerkInstance not found

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,46 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
-import { global } from 'styled-jsx/css'
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+// import { dark } from "@clerk/themes";
 
+import "../globals.css";
+// import LeftSidebar from "@/components/shared/LeftSidebar";
+// import Bottombar from "@/components/shared/Bottombar";
+// import RightSidebar from "@/components/shared/RightSidebar";
+// import Topbar from "@/components/shared/Topbar";
 
-export const metadata = {
-  title: 'Threads',
-  description: 'A NextJS 13 Meta Threads Application'
-}
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
+};
 
-export default function RootLayout(
-  { children } : { children: React.ReactNode })
-{
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} bg-dark-1`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+    >
+      <html lang='en'>
+        <body className={inter.className}>
+          {/* <Topbar /> */}
+
+          <main className='flex flex-row'>
+            {/* <LeftSidebar /> */}
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            {/* <RightSidebar /> */}
+          </main>
+
+          {/* <Bottombar /> */}
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
