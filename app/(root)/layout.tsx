b81d9b88506e0bc6d6ba4fa49ee7e23d2e068322
Avery Yang <averyyang2674@gmail.com>
2023-09-11 18:40:33 -0500

    create components and add assets folder

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 // import { dark } from "@clerk/themes";
 
 import "../globals.css";
-// import LeftSidebar from "@/components/shared/LeftSidebar";
-// import Bottombar from "@/components/shared/Bottombar";
-// import RightSidebar from "@/components/shared/RightSidebar";
-// import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,21 +24,24 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider
+      // appearance={{
+      //   baseTheme: dark,
+      // }}
     >
       <html lang='en'>
         <body className={inter.className}>
-          {/* <Topbar /> */}
+          <Topbar />
 
           <main className='flex flex-row'>
-            {/* <LeftSidebar /> */}
+            <LeftSidebar />
             <section className='main-container'>
               <div className='w-full max-w-4xl'>{children}</div>
             </section>
             {/* @ts-ignore */}
-            {/* <RightSidebar /> */}
+            <RightSidebar />
           </main>
 
-          {/* <Bottombar /> */}
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
