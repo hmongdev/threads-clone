b56b0844502b5fba27e286b59b2d85a52e7ff1b7
Avery Yang <averyyang2674@gmail.com>
2023-09-11 18:16:48 -0500

    updated tsconfig

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,11 +13,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout(
   { children } : { children: React.ReactNode })
 {
-  return (<ClerkProvider>
+  return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className} bg-dark-1`}>
         {children}
       </body>
     </html>
-  </ClerkProvider>)
+    </ClerkProvider>
+  )
 }
