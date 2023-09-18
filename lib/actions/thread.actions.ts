1e7ffdbd6166c73fdd80dba8e3fa8ea43cb7105a
Avery Yang <averyyang2674@gmail.com>
2023-09-18 17:52:14 -0500

    create GET request; ThreadCard component to display on DOM

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -11,6 +11,7 @@ interface Params {
   path: string,
 }
 
+// POST
 export async function createThread({ text, author, communityId, path }: Params
   ) {
     try {
@@ -31,4 +32,40 @@ export async function createThread({ text, author, communityId, path }: Params
     } catch (error: any) {
       throw new Error(`Failed to create thread: ${error.message}`);
     }
-  }
+}
+
+// GET threads + pagination
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  
+  // Calculate number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+  
+  // Fetch posts that have no parents (top-level threads...)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: "_id name parentId image"
+      }
+    })
+  
+  // find top-level threads (no children "comments")
+  const totalPostsCount = await Thread.countDocuments({
+  parentId: {
+    $in:
+      [null, undefined]
+  }})
+      
+const posts = await postsQuery.exec();
+
+const isNext = totalPostsCount > skipAmount + posts.length;
+
+return { posts, isNext };
+}
