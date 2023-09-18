2f62cba65fc9affb118dd5b603dd8bb0f2027996
Avery Yang <averyyang2674@gmail.com>
2023-09-18 18:01:41 -0500

    styled threadcard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from "next/link"
 interface Props {
   id: string;
   currentUserId: string;
@@ -33,8 +35,29 @@ createdAt,
 comments,
 }: Props) => {
   return (
-    <article>
-      <h2 className="text-small-regular text-light-2">
+    <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7'>
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link
+              href={`/profile/${author.id}`}
+              className="relative h-11 w-11"
+            >
+              <Image
+                src={author.image}
+                alt="Profile Image"
+                fill
+                className="cursor-pointer rounded-full"
+              />
+            </Link>
+            
+            <div className="thread-card_bar" />
+          </div>
+        </div>
+      </div>
+      
+      
+      <h2 className="mt-2 text-small-regular text-light-2">
         {content}
       </h2>
     </article>
