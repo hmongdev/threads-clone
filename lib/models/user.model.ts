903fc2d635635cccae30757d45ff72718aa557de
Avery Yang <averyyang2674@gmail.com>
2023-09-21 18:11:25 -0500

    update user model

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,31 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
   image: String,
   bio: String,
   threads: [
-    // one user can have multiple threads
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Thread'
-    }
+      ref: "Thread",
+    },
   ],
   onboarded: {
     type: Boolean,
     default: false,
   },
   communities: [
-    // one user can belong to multiple communities
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Community'
-    }
-  ]
+      ref: "Community",
+    },
+  ],
 });
 
-// if User already exists, use that User
-// if User doesn't exist, create a new one in DB
-export const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
