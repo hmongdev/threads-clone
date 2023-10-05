f508ce79aaf1375e4e821c6cf19052e854fe659f
Steve Yang <steveyangwork@gmail.com>
2023-10-05 14:33:15 -0500

    install bun; modify middleware for /onboarding; update package.json

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -12,23 +12,25 @@ async function Page() {
   if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user.id,
+    id: user?.id,
     objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    username: userInfo ? userInfo?.username : user?.username,
+    name: userInfo ? userInfo?.name : user?.firstName ?? "",
     bio: userInfo ? userInfo?.bio : "",
-    image: userInfo ? userInfo?.image : user.imageUrl,
-  };
+    image: userInfo ? userInfo?.image : user?.imageUrl,
+  }; 
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
       <p className='mt-3 text-base-regular text-light-2'>
-        Complete your profile now, to use Threds.
+        Complete your profile now, to use Threads.
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
-        <AccountProfile user={userData} btnTitle='Continue' />
+        <AccountProfile
+          user={userData}
+          btnTitle='Continue' />
       </section>
     </main>
   );
